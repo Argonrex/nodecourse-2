6d3bc183b0c98a2854d4d2084a5aa76033c8a420
rex <rhexachdheydhan@icloud.com>
2018-08-15 12:19:42 -0400

    Add get todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ app.post('/todos', (req, res) => {
  });
 });
 
+app.get('/todos', (req, res) => {
+ Todo.find().then((todos) => {
+  res.send({todos});
+ }, (e) => {
+  res.send(e);
+ })
+ console.log("done");
+});
+
 app.listen(3000, () => {
  console.log("Started on port 3000");
 });
