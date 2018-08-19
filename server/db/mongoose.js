fed2498a6f6ea0520af83035d8e9fe564e2c56ea
rex <rhexachdheydhan@icloud.com>
2018-08-19 14:37:14 -0400

    Addition of delete

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://rex:Miniclip123@ds227322.mlab.com:27322/rexactest',{ useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://rex:Miniclip123@ds227322.mlab.com:27322/rexactest',{ useNewUrlParser: true });
 
 module.exports =  {mongoose};
