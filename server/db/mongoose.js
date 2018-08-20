9ad922b7d48f64d0e5e8a2be001f0b195c8fc609
rex <rhexachdheydhan@icloud.com>
2018-08-19 21:19:28 -0400

    Update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://rex:Miniclip123@ds227322.mlab.com:27322/rexactest',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp' || 'mongodb://rex:Miniclip123@ds227322.mlab.com:27322/rexactest',{ useNewUrlParser: true });
 
 module.exports =  {mongoose};
